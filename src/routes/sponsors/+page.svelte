f351ddc5b3b91ba8ee18575bb8e342c7ec8e98c0
Najman <najman.husaini@utah.edu>
2023-12-05 18:33:36 -0700

    sponsors

---
+++ src/routes/sponsors/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import ngPhoto from '$lib/assets/ng.png';
+</script>
+
+<title>Sponsors | Utah Student Robotics</title>
+<header>
+	<h1>Sponsors</h1>
+</header>
+<main>
+	<img src={ngPhoto} alt="Northrop Grumman">
+
+	<p>
+		If you would like to be added to this list, our t-shirts, and our robot,
+		please contact Chandler Korbin Millar at <a href="mailto:u0962549@utah.edu">u0962549@utah.edu</a>.
+	</p>
+	
+</main>
+
+<style>
+	h1 {
+		color: black;
+	}
+	main {
+		margin-top: min(9rem, 10vw);
+	}
+	main > img {
+		width: min(48rem, 100vw - 2rem);
+	}
+</style>
